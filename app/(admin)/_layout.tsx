bccc04b221fca4025841e159859769b816e6f668
Kyreena Daruwalla <157062399+kyreena18@users.noreply.github.com>
2025-09-03 01:23:11 +0530

    Updated _layout.tsx

--- app/(admin)/_layout.tsx
+++ app/(admin)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { BarChart3, Users, Settings, Building, GraduationCap, Home } from 'lucide-react-native';
+import { ChartBar as BarChart3, Users, Settings, Building, GraduationCap, Chrome as Home } from 'lucide-react-native';
 
 export default function AdminLayout() {
   return (
