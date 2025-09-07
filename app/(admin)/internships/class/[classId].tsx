2c9b65a1eb26e43b0f8a1cd5cf2b04317000da9d
Kyreena Daruwalla <157062399+kyreena18@users.noreply.github.com>
2025-09-08 03:11:37 +0530

    Updated [classId].tsx

--- app/(admin)/internships/class/[classId].tsx
+++ app/(admin)/internships/class/[classId].tsx
@@ -1,7 +1,7 @@
 import { downloadFileWithFallback } from '@/lib/utils';
 import * as XLSX from 'xlsx';
 import * as WebBrowser from 'expo-web-browser';
-import { createAndDownloadZip, downloadFileWithFallback } from '@/lib/utils';
+import { createAndDownloadZip } from '@/lib/utils';
 
 interface StudentProfile {
 }
